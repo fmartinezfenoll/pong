1db5578f0baac0ce4c44771b626f7bb20727378f
fmartinezfenoll <fmf29@gmail.com>
2024-04-29 15:28:24 +0200

    First commit

---
+++ pong.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// Constantes básicas del juego
+const FRAME_PER_SECOND = 50;
+
+const NUM_BALLS = 5;
+
+const BG_COLOR = 'BLACK';
+
+const FONT_COLOR = 'WHITE';
+const FONT_FAMILY = 'impact';
+const FONT_SIZE = '45px';
+
+const NET_COLOR = 'WHITE';
+const NET_WIDTH = 4;
+const NET_HEIGHT = 10;
+const NET_PADDING = 15;
+
+const PADDLE_RIGHT_COLOR = 'WHITE';
+const PADDLE_LEFT_COLOR = 'WHITE';
+const PADDLE_WIDTH = 20;
+const PADDLE_HEIGHT = 100;
+
+const BALL_COLOR = 'WHITE';
+const BALL_RADIUS = 10;
+const BALL_DELTA_VELOCITY = 0.5;
+const BALL_VELOCITY = 5;
+
+const gameStateEnum = {
+    SYNC: 0,
+    PLAY: 1,
+    PAUSE: 2,
+    END: 3,
+};
