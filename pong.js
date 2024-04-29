9f7380f457e42102d6423b740ad3b6043ddcf5a1
fmartinezfenoll <fmf29@gmail.com>
2024-04-29 15:55:39 +0200

    Pruebas básicas canvas

--- pong.js
+++ pong.js
@@ -32,3 +32,19 @@ const gameStateEnum = {
     PAUSE: 2,
     END: 3,
 };
+// Recuperar el canvas
+const cvs = document.getElementById('pong_canvas');
+const ctx = cvs.getContext('2d');
+
+ctx.fillStyle = 'BLACK';
+ctx.fillRect(0,0,600,400);
+
+ctx.fillStyle = 'WHITE';
+ctx.beginPath();
+ctx.arc(50,60,10,0,2*Math.PI);
+ctx.closePath();
+ctx.fill();
+
+ctx.fillStyle = 'BLUE';
+ctx.font = '45px impact';
+ctx.fillText('Saludos',200,200);
